7fe4af3832fbe58459ac315829c3229b532d6a53
ved-thakre <vedantthakre7@gmail.com>
2024-02-19 22:23:37 +0530

    few changes

--- src/components/MediaItem/index.js
+++ src/components/MediaItem/index.js
@@ -1,10 +1,37 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import Image from "next/image";
 
-const MediaItem = () => {
-  return <div>
-    media Item
-  </div>;
+const base = "https://image.tmdb.org/t/p/w500";
+
+const MediaItem = ({ media, title }) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.8,
+        delay: 0.5,
+        ease: [0, 0.71, 0.2, 1.01],
+      }}
+    >
+      <div
+        className="relative cardWrapper h-28 min-w-[180px] cursor-pointer md:h-36 md:min-w-[260px]
+         transform transition duration-500 hover:scale-110 hover:z-[999]"
+      >
+        <Image
+          src={`${base}${media?.backdrop_path || media?.poster_path}`}
+          alt="media"
+          layout="fill"
+          className="rounded sm object-cover md:rounded hover:rounded-sm"
+        />
+      </div>
+      <div className="space-x-3 hidden absolute p-2 bottom-0 buttonWrapper">
+        <button></button>
+      </div>
+    </motion.div>
+  );
 };
 
 export default MediaItem;
