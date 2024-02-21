f1a955036620461f049ee6658bdff939139bd725
ved-thakre <vedantthakre7@gmail.com>
2024-02-21 22:41:50 +0530

    Fixed parmas issue in search

--- src/app/search/[query]/page.js
+++ src/app/search/[query]/page.js
@@ -3,11 +3,13 @@ import MangeAccounts from "@/components/MangeAccounts";
 import Unauthpage from "@/components/Unauthpage";
 import { GlobalContext } from "@/context";
 import { useSession } from "next-auth/react";
+import { useParams } from "next/navigation";
 import React, { useContext } from "react";
 
 const Search = () => {
   const { data: session } = useSession();
   const { loggedInAccount } = useContext(GlobalContext);
+  const params = useParams();
 
   if (session === null) return <Unauthpage />;
   if (loggedInAccount === null) return <MangeAccounts />;
